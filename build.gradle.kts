a693aadbdc51580a988cad01a8ec5b131cca2a28
Sybsuper <sybsuper@gmail.com>
2025-05-31 22:24:35 +0200

    feat: add Dokka documentation generation and deployment to GitHub Pages

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("com.gradleup.shadow") version "8.3.0"
     id("xyz.jpenilla.run-paper") version "2.3.1"
     kotlin("plugin.serialization") version "2.1.20"
+    id("org.jetbrains.dokka") version "2.0.0"
 }
 
 group = "com.sybsuper"
