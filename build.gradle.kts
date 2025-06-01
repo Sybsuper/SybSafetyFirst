613a72456e688e155855b548dd5dbb03562eaf6c
Sybsuper <sybsuper@gmail.com>
2025-06-01 19:16:57 +0200

    feat: update build configuration to include ProGuard and JVM toolchain support for debugging

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,22 @@
+import proguard.gradle.ProGuardTask
+
 plugins {
+    `jvm-toolchains`
     kotlin("jvm") version "2.2.0-RC"
     id("com.gradleup.shadow") version "8.3.0"
     id("xyz.jpenilla.run-paper") version "2.3.1"
     kotlin("plugin.serialization") version "2.1.20"
     id("org.jetbrains.dokka") version "2.0.0"
+
+}
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.guardsquare:proguard-gradle:7.7.0")
+    }
 }
 
 group = "com.sybsuper"
@@ -25,29 +38,51 @@ dependencies {
     implementation("com.charleskorn.kaml:kaml:0.78.0")
 }
 
-tasks {
-    runServer {
-        // Configure the Minecraft version for our task.
-        // This is the only required configuration besides applying the plugin.
-        // Your plugin's jar (or shadowJar if present) will be used automatically.
-        minecraftVersion("1.21.5")
-    }
-}
-
 val targetJavaVersion = 21
 kotlin {
     jvmToolchain(targetJavaVersion)
 }
 
-tasks.build {
-    dependsOn("shadowJar")
+java {
+    toolchain {
+        vendor = JvmVendorSpec.JETBRAINS
+        languageVersion = JavaLanguageVersion.of(targetJavaVersion)
+    }
 }
 
-tasks.processResources {
-    val props = mapOf("version" to version)
-    inputs.properties(props)
-    filteringCharset = "UTF-8"
-    filesMatching("plugin.yml") {
-        expand(props)
+tasks {
+    runServer {
+        minecraftVersion("1.21.5")
+        jvmArgs("-XX:+AllowEnhancedClassRedefinition")
+    }
+
+    build {
+        dependsOn("shadowJar", "proguardJar")
+    }
+
+    processResources {
+        val props = mapOf("version" to version)
+        inputs.properties(props)
+        filteringCharset = "UTF-8"
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
+    }
+
+    register<ProGuardTask>("proguardJar") {
+        dontwarn()
+        dontoptimize()
+        verbose()
+        libraryjars(
+            mapOf(
+                "jarFilter" to "!**.jar",
+                "filter" to "!module-info.class"
+            ), "${System.getProperty("java.home")}/jmods/java.base.jmod"
+        )
+        libraryjars(configurations.compileClasspath.get().files)
+        keepattributes("*Annotation*")
+        keep("public class com.sybsuper.sybsafetyfirst.** { @** *; }")
+        injars(shadowJar.get().archiveFile)
+        outjars("${layout.buildDirectory.get()}/libs/${project.name}-${project.version}-min.jar")
     }
 }
